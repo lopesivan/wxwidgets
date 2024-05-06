ca5a6d8338e5e0b465d0646a79586e7b6860d4b1
Ivan Lopes <lopesivan.ufrj@gmail.com>
2024-05-06 06:34:00 -0300

    Adiciona o arquivo 01/.clang-format

--- 01/main.cpp
+++ 01/main.cpp
@@ -1,24 +1,25 @@
 #include <wx/wx.h>
 
 class MyApp : public wxApp {
-public:
-  virtual bool OnInit();
+    public:
+    virtual bool OnInit();
 };
 
 class MyFrame : public wxFrame {
-public:
-  MyFrame(const wxString &title);
+    public:
+    MyFrame(const wxString& title);
 };
 
 wxIMPLEMENT_APP(MyApp);
 
 bool MyApp::OnInit() {
-  MyFrame *frame = new MyFrame("Hello wxWidgets");
-  frame->Show(true);
-  return true;
+    MyFrame* frame = new MyFrame("Hello wxWidgets");  // sdsdsd
+    frame->Show(true);                                // asasasas
+    return true;                                      // ssdsd
 }
 
-MyFrame::MyFrame(const wxString &title) : wxFrame(NULL, wxID_ANY, title) {
-  wxPanel *panel = new wxPanel(this, wxID_ANY);
-  wxButton *button = new wxButton(panel, wxID_ANY, "Click Me", wxPoint(20, 20));
+MyFrame::MyFrame(const wxString& title) : wxFrame(NULL, wxID_ANY, title) {
+    wxPanel* panel = new wxPanel(this, wxID_ANY);
+    wxButton* button =
+        new wxButton(panel, wxID_ANY, "Click Me", wxPoint(20, 20));
 }
